6b0bfb7e392f359754903ba0c9e1da8f0c114250
Oktikk <65051521+Oktikk@users.noreply.github.com>
2022-08-05 02:50:49 +0200

    Progress to page 144

--- Assets/Scripts/Monobehaviours/Player.cs
+++ Assets/Scripts/Monobehaviours/Player.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Player : Character
 {
+    public HealthBar healthBarPrefab;
+
+    HealthBar healthBar;
+
+    private void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+
+        healthBar.character = this;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CanBePickedUp"))
@@ -14,25 +25,39 @@ public class Player : Character
             {
                 print("Kolizja " + hitObject.objectName);
 
+                bool shouldDisappear = false;
+
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
+                        shouldDisappear = true;
                         break;
                     case Item.ItemType.HEALTH:
-                        AdjustHitPoints(hitObject.quantity);
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
                         break;
                     default:
                         break;
                 }
 
-                collision.gameObject.SetActive(false);
+                if (shouldDisappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
             }
         }
     }
 
-    void AdjustHitPoints(int amount)
+    public bool AdjustHitPoints(int amount)
     {
-        hitPoints = hitPoints + amount;
-        print("Nowe punkty: " + amount + ". Razem: " + hitPoints);
+        if(hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+
+            print("Nowe punkty: " + amount + ". Razem: " + hitPoints.value);
+            
+            return true;
+        }
+
+        return false;
     }
 }
